04c02f77818071e52815c3ee882956317e506a76
Yewtree <banchiyong@gmail.com>
2020-02-27 14:56:21 +0900

    #2 complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
+const foodILike = [
+
+  {    
+    id : 1,
+    name : "Kimchi",
+    image : "http://minamkimchi.com/web/product/big/201606/18_shop1_290903.jpg",
+    rating : 3.5
+  },
+
+  {    
+    id : 2,
+    name : "Donkachu",
+    image : "http://image.auction.co.kr/itemimage/10/dd/61/10dd6169a6.jpg",
+    rating : 4.8
+  }
+
+];
+
+function Food({name,image,rating}) {  
+  return (
+  <div>
+    <h1> I like {name}</h1>
+    <h4> {rating}/5.0 </h4>
+    <img src={image} alt={name}/>
+  </div>);
+}
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {foodILike.map(dish => (
+      <Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating} /> 
+      ))}
+      
     </div>
   );
 }
